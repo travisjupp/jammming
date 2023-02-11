17c06fb46787d4f42306b4f29e65d0cbc3641a19
tjupp <travisjupp@gmail.com>
2023-02-10 17:39:12 -0700

    App.js placeholder data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,52 @@ import { Playlist } from '../Playlist/Playlist';
 
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'track01',
+          artist: 'artist01',
+          album: 'album01',
+          id: 'id01'
+        },
+        {
+          name: 'track02',
+          artist: 'artist02',
+          album: 'album02',
+          id: 'id02'
+        },
+        {
+          name: 'track03',
+          artist: 'artist03',
+          album: 'album03',
+          id: 'id03'
+        }
+      ],
+      playlistName: 'Custom Playlist',
+      playlistTracks: [
+        {
+          name: 'customTrack01',
+          artist: 'customArtist01',
+          album: 'customAlbum01',
+          id: 'customID01'
+        },
+        {
+          name: 'customTrack02',
+          artist: 'customArtist02',
+          album: 'customAlbum02',
+          id: 'customID02'
+        },
+        {
+          name: 'customTrack03',
+          artist: 'customArtist03',
+          album: 'customAlbum03',
+          id: 'customID03'
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
@@ -17,7 +62,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             {/* <!-- Add a SearchResults component --> */}
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             {/* <!-- Add a Playlist component --> */}
             <Playlist />
           </div>
