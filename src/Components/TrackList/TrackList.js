88c75d38bf5124a4d3e4eb5ccf1350c869a28e39
tjupp <travisjupp@gmail.com>
2023-04-17 09:09:21 -0600

    Feat: move tracks
    pass props to first/last tracks

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -9,10 +9,22 @@ export class TrackList extends React.Component {
         return (
             <div className="TrackList">
                 {/* <!-- You will add a map method that renders a set of Track components  --> */}
-                {this.props.tracks.map(track => <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} >{track}</Track>)}
-                {/* <Track onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} /> */}
+                {this.props.tracks.map(track => {
+
+                    console.log('first', track.name, this.props.tracks.indexOf(track) === 0);
+                    console.log('last', track.name, this.props.tracks.indexOf(track) === this.props.tracks.length - 1);
+
+
+                    // tag first / last track in array using props
+                    return this.props.tracks.indexOf(track) === 0 ?
+                        <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} firstTrack={true} >{track}</Track> :
+                        this.props.tracks.indexOf(track) === this.props.tracks.length - 1 ?
+                            <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} lastTrack={true} >{track}</Track> :
+                            <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} >{track}</Track>
+
+                    // return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} >{track}</Track>
+                })}
             </div>
         );
     }
 }
-
